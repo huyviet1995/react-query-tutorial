e2c27d1bf663d7384271554bd50b29051c1674b7
Viet <viet.dang@dirox.net>
2022-06-06 22:16:58 +0700

    Extracting the pagination variables

--- src/Characters.jsx
+++ src/Characters.jsx
@@ -1,15 +1,19 @@
+import { useState } from 'react';
 import { useQuery } from 'react-query';
 import { Character } from './components/Character';
 
 export const Characters = props => {
-    const fetchCharacters = async () => {
-        return await fetch('https://rickandmortyapi.com/api/character')
+
+    const [page, setPage] = useState(3);
+
+    const fetchCharacters = async ({ queryKey }) => {
+        return await fetch(`https://rickandmortyapi.com/api/character?page=${queryKey[1]}`)
             .then(response => {
                 return response.json();
             })
     }
 
-    const { data, status } = useQuery("characters", fetchCharacters);
+    const { data, status } = useQuery(["characters", page], fetchCharacters);
 
     if (status === 'loading') {
         return (
@@ -27,13 +31,13 @@ export const Characters = props => {
         )
     }
 
-return (
-    <div className="characters">
-        {data.results.map(character => (
-            <Character character={character} />
-        ))}
-    </div>
-)
+    return (
+        <div className="characters">
+            {data.results.map((character, index) => (
+                <Character key={index} character={character} />
+            ))}
+        </div>
+    )
 }
 
 export default Characters;
